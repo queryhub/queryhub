876da93d16edaae8616ac6e47cb9c13b698d88ac
Diego Rocha <dhsrocha@gmail.com>
2019-10-25 09:47:23 -0300

    Restricted test module's visibility to only test package.

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -2,9 +2,11 @@
  * @author <a href="mailto:queryhub.pub@gmail.com">Diego Rocha</a>
  * @since 0.1.0
  */
-open module test.module {
+module test.module {
 
   requires prod.module;
 
   requires org.junit.jupiter.api;
+
+  opens org.queryhub.test to org.junit.jupiter.api;
 }
